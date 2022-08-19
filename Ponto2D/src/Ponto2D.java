6fcb177ee3652c1e83517beee9ae865c3611fe0f
jramso <josue.rsou2@gmail.com>
2022-08-19 01:39:49 -0300

    CONFIRME

---
+++ Ponto2D/src/Ponto2D.java
@@ -0,0 +1,32 @@
+public class Ponto2D {
+
+    private int x;
+    private int y;
+    /* Constructor */
+    public Ponto2D(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    /* METODOS */
+    public float distancia(Ponto2D pontoB){
+        /* calcular distancia entre 2 pontos (Math devolve double)*/
+        /* ­↓↓↓↓↓ Fomula distancia entre 2 pontos ↓↓↓
+        dist = ((x-xB)²+(y-yB)²))**1/2 */
+        return (float) Math.sqrt(Math.pow(x-pontoB.getX(),2)+Math.pow(y-pontoB.getY(), 2));
+    }
+    public boolean igual(Ponto2D pontoB){
+        return x==pontoB.getX() && y==pontoB.getY();
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+
+    public String toString(){
+        return "X= "+getX()+" Y= "+getY();
+    }
+}
---
+++ Ponto2D/src/Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+    public static void main(String[] args) {
+        Ponto2D a = new Ponto2D(5, 3);
+        Ponto2D b = new Ponto2D(7, 4);
+
+        System.out.println("Distancia entre o PontoA ("+a.getX()+","+a.getY()+ ") e PontoB ("+b.getX()+","+b.getY()+"): "+String.format("%.2f",a.distancia(b)));
+        System.out.println("O PontoA e o PontoB sao iguais? "+String.format(a.igual(b)?"Sim":"Não"));
+        System.out.println("PontoA :"+a.toString());
+        System.out.println("PontoB :"+b.toString());
+
+        // System.out.println("Distancia entre pontoA e pontoB "+a.toString());
+    }
+}
