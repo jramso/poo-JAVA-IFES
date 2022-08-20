463eea5c205d93d743cbe0b1528a5586bfca3958
jramso <josue.rsou2@gmail.com>
2022-08-20 20:27:47 -0300

    confirme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # poo-JAVA-IFES
 
-[#Eq2g:Equação de segundo grau](Eq2g.java)
+Atividades e exercícios da disciplina de POO - Ifes Campus Serra
+@Jramso - Josué Ramos Souza - 4ºPeríodo
+
+
+Lista 1:
+[1: Ponto2D:Ponto no Plano](Ponto2D/src/)
+[2: Eq2g:Equação de segundo grau](Eq2g/src/)
---
+++ Retangulo/src/Retangulo.java
@@ -0,0 +1,40 @@
+
+public class Retangulo {
+    private Ponto2D vS1; // vertice superior 1   1  2
+    private Ponto2D vS2; // vertice superior 2   3  4
+    private Ponto2D vI3; // inferior 3
+    private Ponto2D vI4; // inferior 4
+
+    public Retangulo(Ponto2D vS1, Ponto2D vI4){
+        vS1=this.vS1;
+        vI4=this.vI4;
+        vI3= new Ponto2D(vI4.getX(),vS1.getY());
+        vS2= new Ponto2D(vS1.getX(),vI4.getY());
+    }
+    public Ponto2D getVS1() {
+        return vS1;
+    }
+    public Ponto2D getvS2() {
+        return vS2;
+    }
+    public Ponto2D getV3() {
+        return vI3;
+    }
+    public Ponto2D getvI4() {
+        return vI4;
+    }
+    public Double area(){
+        return vS1.distancia(vS2) *vS1.distancia(vI3);
+    }
+    public Double perimetro(){
+        return 2*(vS1.distancia(vS2) + vS1.distancia(vI3));
+    }
+    public boolean pertence(Ponto2D ponto){
+        return ponto.getY()<=vS1.getY() && ponto.getY()>=vI3.getY() && ponto.getX()>=vI3.getX() && ponto.getX()<=vS2.getX();
+    }
+    
+    public String toString(){
+        return "V1:"+vS1.toString()+" V2:"+vS2.toString()+" V3:"+vI3.toString()+" V4:"+vI4.toString();
+    }
+
+}
