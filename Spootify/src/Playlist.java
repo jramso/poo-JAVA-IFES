4c82b87a7cfd7a54122611f9831b74e9a0303a83
jramso <josue.rsou2@gmail.com>
2022-09-12 22:54:54 -0300

    check

---
+++ Spootify/src/Playlist.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+
+public class Playlist {
+    
+
+    private String nome;
+    private int horas;
+    private int minutos;
+    private int segundo;
+    ArrayList<Conteudo> Plist;
+    
+    public Playlist(String nomePL,int horas, int min, int sec){
+        this.nome=nomePL;
+        this.horas=horas;
+        this.minutos=min;
+        this.segundo=sec;
+        ArrayList<Conteudo> Plist = new ArrayList<>();
+    }
+
+
+    public int calcTempo(){
+        return 0;
+    }
+    public ArrayList<Conteudo> listaMsc(){
+        ArrayList<Conteudo> MusLst = new ArrayList<>();
+        for(Conteudo mus:Plist){
+            if(mus.getClass().getSimpleName()=="Musica"){
+                MusLst.add(mus);
+            }
+        }
+        return MusLst;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public int getHoras() {
+        return horas;
+    }
+
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+
+    public void setSegundo(int segundo) {
+        this.segundo = segundo;
+    }
+
+
+    public ArrayList<Conteudo> getPlist() {
+        return Plist;
+    }
+
+
+    public void setPlist(ArrayList<Conteudo> plist) {
+        Plist = plist;
+    }
+
+
+
+    gets
+}
