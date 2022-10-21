1f8c4e3820684c7f32c359d1e5ceece520a6e5d1
jramso <josue.rsou2@gmail.com>
2022-10-21 14:14:20 +0000

    ✨ feat(Performance): getters
    criando getters

--- POOflix/src/domain/Performance.java
+++ POOflix/src/domain/Performance.java
@@ -2,4 +2,14 @@ package POOflix.src.domain;
 
 public class Performance {
     
+
+    public Performance(int idep, int idepers, Ator ator){
+
+    }
+    /* 
+    public int getidep();
+    public int getidepers();
+    public Ator getAtor();
+
+    */
 }
