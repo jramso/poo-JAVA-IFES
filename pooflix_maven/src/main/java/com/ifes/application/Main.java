f1868901e92ef0c1b8791dbc61bd7789bee1dd4c
jramso <josue.rsou2@gmail.com>
2022-11-29 19:42:28 +0000

    ✨ feat(mainClass POOflixMaven): Main
    Comment EntityManager
    
    FIX: errors on Hibernate

--- pooflix_maven/src/main/java/com/ifes/application/Main.java
+++ pooflix_maven/src/main/java/com/ifes/application/Main.java
@@ -1,14 +1,14 @@
 package com.ifes.application;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+// import jakarta.persistence.EntityManager;
+// import jakarta.persistence.EntityManagerFactory;
+// import jakarta.persistence.Persistence;
 
-import com.ifes.dominio.*;
+//import com.ifes.dominio.*;
 
 public class Main {
-	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("pooflix_maven");
-	private static EntityManager entityManager = emf.createEntityManager();
+	//private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("pooflix_maven");
+	//private static EntityManager entityManager = emf.createEntityManager();
 
 	public static void main(String[] args) {
 		// Ator ator = entityManager.find(Ator.class, 1);
